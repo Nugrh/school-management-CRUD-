fd89e0d17cbac5c6adb8097ab466001e2b7f6dd5
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2021-12-25 20:45:13 +0700

    add_edit_delete_route

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,20 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 // Admin
 Route::prefix('student')->group(function () {
     Route::get('/', [StudentController::class, 'index'])->name('student');
+    Route::get('/edit/{id}/{student_id}', [StudentController::class, 'edit'])->name('student.edit');
+    Route::get('/delete/{id}/{student_id}', [StudentController::class, 'destroy'])->name('student.destroy');
+
     Route::post('/store', [StudentController::class, 'store'])->name('student.store');
+    Route::post('/update', [StudentController::class, 'update'])->name('student.update');
 });
 
 Route::prefix('teacher')->group(function () {
     Route::get('/', [TeacherController::class, 'index'])->name('teacher');
+    Route::get('/edit/{id}/{teacher_id}', [TeacherController::class, 'edit'])->name('teacher.edit');
+    Route::get('/delete/{id}/{teacher_id}', [TeacherController::class, 'destroy'])->name('teacher.destroy');
+
     Route::post('/store', [TeacherController::class, 'store'])->name('teacher.store');
+    Route::post('/update', [TeacherController::class, 'update'])->name('teacher.update');
 });
 
 Auth::routes();
