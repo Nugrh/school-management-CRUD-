cea64c99cd6c75f310df7bac61a0f0e0ff899c84
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2021-12-25 20:44:38 +0700

    update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -17,9 +17,9 @@
         <p>Teacher</p>
     </a>
 </li>
-<li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link active">
-        <i class="nav-icon fas fa-file"></i>
-        <p>Attendance</p>
-    </a>
-</li>
+{{--<li class="nav-item">--}}
+{{--    <a href="{{ route('home') }}" class="nav-link active">--}}
+{{--        <i class="nav-icon fas fa-file"></i>--}}
+{{--        <p>Attendance</p>--}}
+{{--    </a>--}}
+{{--</li>--}}
