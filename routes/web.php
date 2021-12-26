c9ffd7a03a41fd9b6f17da6e63cc38ad7001c830
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2021-12-26 16:24:01 +0700

    route_search

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::prefix('student')->group(function () {
     Route::get('/', [StudentController::class, 'index'])->name('student');
     Route::get('/edit/{id}/{student_id}', [StudentController::class, 'edit'])->name('student.edit');
     Route::get('/delete/{id}/{student_id}', [StudentController::class, 'destroy'])->name('student.destroy');
+    Route::get('/search', [StudentController::class, 'search'])->name('student.search');
 
     Route::post('/store', [StudentController::class, 'store'])->name('student.store');
     Route::post('/update', [StudentController::class, 'update'])->name('student.update');
@@ -36,6 +37,7 @@ Route::prefix('teacher')->group(function () {
     Route::get('/', [TeacherController::class, 'index'])->name('teacher');
     Route::get('/edit/{id}/{teacher_id}', [TeacherController::class, 'edit'])->name('teacher.edit');
     Route::get('/delete/{id}/{teacher_id}', [TeacherController::class, 'destroy'])->name('teacher.destroy');
+    Route::get('/search', [TeacherController::class, 'search'])->name('teacher.search');
 
     Route::post('/store', [TeacherController::class, 'store'])->name('teacher.store');
     Route::post('/update', [TeacherController::class, 'update'])->name('teacher.update');
